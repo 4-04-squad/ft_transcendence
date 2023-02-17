a9533281b6ff227aadd64612cada8cc0b02846d0
Martin Jean-Christio <admin@marjc.ch>
2023-02-17 13:47:41 +0100

    🏗️ Init PostgresSQL database + Adminer

---
+++ project/src/backend/scripts/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# This script is used to run the application in a docker container.
+
+# Wait for the app to be mounted
+while [ ! -d /app ]; do
+    sleep 1
+done
+
+# Install dependencies
+cd /app && yarn install && yarn build
+
+# Start the app
+yarn start
