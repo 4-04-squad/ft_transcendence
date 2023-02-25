7dc874847ef1d3d770878a173792431e23a0f67b
Martin Jean-Christio <admin@marjc.ch>
2023-02-25 19:01:53 +0100

    Begin fix

---
+++ project/scripts/setup.unix.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# This script is used to clone the project and install dependencies.
+
+# Ask if pull the latest changes if the repository is already cloned
+if [ -f "./src/backend/app/package.json" ]; then
+    read -p "Do you want to pull the latest changes from the backend repository? [y/n] " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        cd ./src/backend/app && git pull
+    fi
+elif [ ! -f "./src/backend/app/package.json" ]; then
+  # Clone backend
+  rm -rf ./src/backend/app && \
+       git clone https://github.com/4-04-squad/backend.git ./src/backend/app
+else
+  echo "Project already initialized."
+  exit 1
+fi
+
+if [ -f "./src/web/app/package.json" ]; then
+    read -p "Do you want to pull the latest changes from the web repository? [y/n] " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        cd ./src/web/app && git pull
+    fi
+elif [ ! -f "./src/web/app/package.json" ]; then
+  # Clone frontend
+  rm -rf ./src/web/app && \
+      git clone https://github.com/4-04-squad/web.git ./src/web/app
+else
+  echo "Project already initialized."
+  exit 1
+fi
