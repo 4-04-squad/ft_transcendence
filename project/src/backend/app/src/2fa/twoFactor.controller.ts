3e9bca0c45bb163334815c86464076a7086ff79b
Lucas Dominique <lucasdominique99@gmail.com>
2023-04-22 10:54:54 +0200

    feat(2fa): Made a migration for two factor auth (#16)
    * feat(2fa): Made a migration for two factor auth
    
    * feat(2fa): WIP-now able to generate a QR code

---
+++ project/src/backend/app/src/2fa/twoFactor.controller.ts
@@ -0,0 +1,35 @@
+import {
+	Controller,
+	Post,
+	Res,
+	UseGuards,
+	Req,
+} from '@nestjs/common';
+import { AuthMiddleware } from 'src/users/users.middleware';
+import { TwoFactorAuthenticationService } from './twoFactor.service';
+import { Response } from 'express';
+import { JwtAuthGuard } from 'src/auth/jwt.guard';
+import { RequestWithUser } from 'src/interfaces/request-with-user.interface';
+import { ApiTags } from '@nestjs/swagger';
+
+@Controller('2fa')
+@ApiTags('2fa')
+export class TwoFactorAuthenticationController {
+	constructor(
+		private readonly twoFactorAuthenticationService: TwoFactorAuthenticationService,
+		private authMiddleware: AuthMiddleware,
+	) { }
+
+	@Post('generate')
+	async register(@Res() res: Response, @Req() request: RequestWithUser) {
+		await new Promise(resolve => this.authMiddleware.use(request, res, resolve));
+		const user = request.user;
+		if (!user) {
+			res.status(401).send({ message: 'Unauthorized' });
+		  } else {
+			const { otpauthUrl } = await this.twoFactorAuthenticationService.generateTwoFactorAuthenticationSecret(request.user);
+
+			return this.twoFactorAuthenticationService.pipeQrCodeStream(res, otpauthUrl);
+		  }
+	}
+}
