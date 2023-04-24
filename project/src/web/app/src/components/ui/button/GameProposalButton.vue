a57a39a7c33ba0c199d740e9590cd571a9f64d45
yace1 <76259226+yace1@users.noreply.github.com>
2023-04-24 22:52:12 +0000

    feat(games): joining games from user card and creating services (#18)
    * feat(games): join game from game list
    
    * feat(games): joining games from user card
    
    * fix(games): remove duplicate lines
    
    * feat(games): add filter my games and protect join
    
    * feat(game): filter list by pseudo and simplify code

--- project/src/web/app/src/components/ui/button/GameProposalButton.vue
+++ project/src/web/app/src/components/ui/button/GameProposalButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="requestGameParty" :class="class">
+  <button @click="gameProposal" :class="class">
     <slot></slot>
   </button>
 </template>
@@ -7,40 +7,68 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { useUserStore } from "@/stores/user";
+import { createGame, joinGame, getGamesByUser } from "@/services/gameServices";
 import axios from "axios";
 import router from "@/router";
 
 export default defineComponent({
   name: "GameProposalButton",
   props: {
-    class: {
-      type: String,
-      default: "",
+      class: {
+        type: String,
+        default: "",
+      },
+      user: {
+        type: String,
+        default: "",
+        required: true,
+      },
     },
-  },
-  setup() {
+  setup(props) {
     const userStore = useUserStore();
+    const user = props.user;
 
+    const createGameAndNavigate = () => {
+      createGame()
+        .then((res) => {
+          router.push({ name: "game", params: { id: res.data.games.id } });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+    const joinGameAndNavigate = (gameId: number) => {
+      joinGame(gameId)
+        .then((res) => {
+          router.push({ name: "game", params: { id: res.data.games.id } });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+    const gameProposal = () => {
+      getGamesByUser(user)
+        .then((res) => {
+          const waiting_games = res.data.games.filter((game: any) => game.status === 'WAITING');
+          if (waiting_games.length > 0) {
+            joinGameAndNavigate(waiting_games[0].id);
+          } else {
+            createGameAndNavigate();
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+ 
     return {
       userStore,
+      createGameAndNavigate,
+      joinGameAndNavigate,
+      gameProposal,
     };
-  },
-  methods: {
-    async requestGameParty() {
-      try {
-        axios
-          .post(`${import.meta.env.VITE_APP_API_URL}/games/create`)
-          .then((res) => {
-            console.log(res);
-            router.push({ name: "game", params: { id: res.data.games.id } });
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      } catch (error: any) {
-        console.log(error);
-      }
-    },
   },
 });
 </script>
