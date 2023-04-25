33651552c1d8b5f2e39f733cdc6e7107eca7dfaf
yace1 <yacinbent@gmail.com>
2023-04-26 00:23:11 +0200

    feat(games): add player list to game layout

--- project/src/web/app/src/components/layout/layout/GameLayout.vue
+++ project/src/web/app/src/components/layout/layout/GameLayout.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="main-game-layout">
+  <div class="main-layout">
     <Header />
     <slot></slot>
+    <Sidebar>
+      <UsersList title="Utilisateurs en ligne" v-if="isLayout"/>
+      <PlayersList title="Players" v-else/>
+    </Sidebar>
   </div>
 </template>
 
 <script lang="ts">
 import Header from "@/components/layout/header/Header.vue";
-import { defineComponent } from "vue";
+import Sidebar from "@/components/layout/sidebar/Sidebar.vue";
+import UsersList from "@/components/user/UsersList.vue";
+import PlayersList from "@/components/user/playersList.vue";
+import { defineComponent, ref, watch } from "vue";
+import { useRoute } from "vue-router";
 
 export default defineComponent({
   name: "GameLayout",
   components: {
     Header,
+    Sidebar,
+    UsersList,
+    PlayersList,
+  },
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    subtitle: {
+      type: String,
+      default: "",
+    },
+  },
+  setup() {
+
+    // watch for route changes
+    const route = useRoute();
+    const isLayout = ref(true);
+
+    // if route contains a game id, then we are in a channel
+    watch(route, () => {
+      if (route.params.id) {
+        isLayout.value = false;
+      } else {
+        isLayout.value = true;
+      }}, { immediate: true }
+    );
+
+    return {
+      isLayout
+    };
   },
 });
 </script>
