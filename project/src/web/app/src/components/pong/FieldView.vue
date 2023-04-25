b458c87d5f3f7ca3c27de9c0554f80a45bfcd65f
Thibaud Paquier <TP.Titus@hotmail.fr>
2023-04-25 12:07:12 +0200

    ADD(gameplay): move player juste one by one not connected to socket

--- project/src/web/app/src/components/pong/FieldView.vue
+++ project/src/web/app/src/components/pong/FieldView.vue
@@ -12,26 +12,36 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
 	name: "FieldView",
-	setup() {
+	data() {
 		return {
 			context: {},
 			ball: {
-				x: (window.innerWidth - 260) / 2 - 5,
-				y: (window.innerHeight - 145) / 2
+				x: (window.innerWidth - 260) / 2 - 10,
+				y: (window.innerHeight - 145) / 2,
+				velocity: 0
 			},
 			score: {
 				p1: 0,
-				p2: 0
+				p2: 0,
+				max_score: 9
 			},
-			positionp1: {
+			player1: {
+				me: 0,
+				speed: 10,
+				tile: 75,
 				x: 0,
 				y: 0
 			},
-			positionp2: {
+			player2: {
+				me: 0,
+				speed: 10,
+				tile: 75,
 				x: 0,
 				y: 0
 			}
 		}
+	},
+	setup() {
 	},
 	methods: {
 	},
@@ -42,38 +52,87 @@ export default defineComponent({
 
 		// choose color
 		let body = document.getElementsByTagName('body')[0];
-		if (body.classList.contains(".dark"))
-			this.context.fillStyle = "white";
+		if (body.classList.contains("dark"))
+			this.context.fillStyle = 'white';
 		else
-			this.context.fillStyle = "black";
-
-		// Background
-		//this.context.fillStyle = "white";
-		//this.context.fillRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+			this.context.fillStyle = 'black';
 
 		// Set line in background
 		this.context.fillRect(this.context.canvas.width / 2, 0, 1, this.context.canvas.height);
 
 		//set spawn point for each player
-		this.positionp1.x = 10;
-		this.positionp1.y = this.context.canvas.height / 2 - 25;
+		this.player1.x = 10;
+		this.player1.y = this.context.canvas.height / 2 - 25;
 		
-		this.positionp2.x = this.context.canvas.width - 20;
-		this.positionp2.y = this.context.canvas.height / 2 - 25;
+		this.player2.x = this.context.canvas.width - 20;
+		this.player2.y = this.context.canvas.height / 2 - 25;
 		
 		// Player 1
-		this.context.fillRect(this.positionp1.x, this.positionp1.y, 10, 75);
+		this.context.fillRect(this.player1.x, this.player1.y, 10, 75);
+		window.addEventListener("keydown", (event) => {
+				if (event.defaultPrevented) {
+				return;
+    		}
+			switch (event.key) {
+				case "s":
+					this.context.clearRect(this.player1.x, 0, 10, this.context.canvas.height);
+					if ((this.player1.y - this.player1.speed) >= (this.context.canvas.height - (this.player1.tile + 25)))
+						this.player1.y = this.context.canvas.height - this.player1.tile;
+					else
+						this.player1.y += this.player1.speed;
+					break;
+				case "w":
+					this.context.clearRect(this.player1.x, 0, 10, this.context.canvas.height);
+					if ((this.player1.y - this.player1.speed) <= 0)
+						this.player1.y = 0;
+					else
+						this.player1.y -= this.player1.speed;
+					break;
+				default:
+					return;
+			}
+			event.preventDefault();
+			this.context.fillRect(this.player1.x, this.player1.y, 10, this.player1.tile);	
+		},
+		);
 
 		// Player 2
-		this.context.fillRect(this.positionp2.x, this.positionp2.y, 10, 75);
+		this.context.clearRect(this.player2.x, 0, 10, this.context.canvas.height);
+		this.context.fillRect(this.player2.x, this.player2.y, 10, 75);
+		window.addEventListener("keydown", (event) => {
+				if (event.defaultPrevented) {
+				return;
+    		}
+			switch (event.key) {
+				case "l":
+					this.context.clearRect(this.player2.x, 0, 10, this.context.canvas.height);
+					if ((this.player2.y - this.player2.speed) >= (this.context.canvas.height - (this.player2.tile + 25)))
+						this.player2.y = this.context.canvas.height - this.player2.tile;
+					else
+						this.player2.y += this.player2.speed;
+					break;
+				case "o":
+					this.context.clearRect(this.player2.x, 0, 10, this.context.canvas.height);
+					if ((this.player2.y - this.player2.speed) <= 0)
+						this.player2.y = 0;
+					else
+						this.player2.y -= this.player2.speed;
+					break;
+				default:
+					return;
+			}
+			event.preventDefault();
+			this.context.fillRect(this.player2.x, this.player2.y, 10, this.player2.tile);	
+		},
+		);
 
 		// Draw score
-		this.context.font = '48px arial';
-		this.context.fillText(this.score.p1, this.context.canvas.width / 2 - 10 - 50, 50);
-		this.context.fillText(this.score.p2, this.context.canvas.width / 2 - 10 + 50, 50);
+		this.context.font = '48px rog fonts';
+		this.context.fillText(this.score.p1, this.context.canvas.width / 2 - 41 - 10, 50);
+		this.context.fillText(this.score.p2, this.context.canvas.width / 2 + 10, 50);
 
-		// Draw Ball
-		this.context.fillRect(this.ball.x, this.ball.y, 10, 10)
+		// Ball
+		this.context.fillRect(this.ball.x, this.ball.y, 20, 20)
 	},
 });
 		/*
