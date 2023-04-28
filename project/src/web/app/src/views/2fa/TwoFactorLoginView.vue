b9f614db05597df83679a217a119d3d9f96ce09f
Lucas Dominique <lucasdominique99@gmail.com>
2023-04-28 13:05:54 +0200

    feat(2fa): Better visual styling

--- project/src/web/app/src/views/2fa/TwoFactorLoginView.vue
+++ project/src/web/app/src/views/2fa/TwoFactorLoginView.vue
@@ -1,26 +1,39 @@
 <template>
-	<MainLayout>
-		<div id="two-factor">
-			<div class="content-wrapper flex flex-col items-center">
-				<div class="flex flex-col items-center mt-4">
-					<div class="flex justify-center mt-4">
-						<!-- Use computed property to generate input fields -->
-						<input v-for="(code, index) in confirmationCode" :key="index"
-							class="h-8 w-8 rounded-md text-white shadow-lg focus:outline-none bg-gradient-to-br from-[#36373a] to-[#3d3f42] text-center mx-1"
-							v-model="confirmationCode[index]" type="text" :maxlength="1" ref="codeRef(index)" required />
-					</div>
-					<button @click.prevent="confirm"
-						class="btn text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-4">
-						Confirm
-					</button>
+  <TwoFactorLayout>
+    <div id="two-factor" class="main-page-content">
+      <div class="content-wrapper flex flex-col items-center justify-center min-h-full">
+				<div class="text-center">
+          <h2 class="text-2xl font-bold mb-4">Please enter the 2FA code</h2>
 				</div>
-			</div>
-		</div>
-	</MainLayout>
+        <div class="flex flex-col items-center mt-4">
+          <div class="flex justify-center mt-4">
+            <!-- Use computed property to generate input fields -->
+            <input
+              v-for="(code, index) in confirmationCode"
+              :key="index"
+              class="h-8 w-8 rounded-md text-white shadow-lg focus:outline-none bg-gradient-to-br from-[#36373a] to-[#3d3f42] text-center mx-1"
+              v-model="confirmationCode[index]"
+              type="text"
+              :maxlength="1"
+              ref="codeRef(index)"
+              required
+            />
+          </div>
+          <button
+            @click.prevent="confirm"
+            class="btn text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-4"
+          >
+            Confirm
+          </button>
+        </div>
+      </div>
+    </div>
+  </TwoFactorLayout>
 </template>
 
+
 <script lang="ts">
-import MainLayout from "@/components/layout/layout/MainLayout.vue";
+import TwoFactorLayout from "@/components/layout/layout/TwoFactorLayout.vue";
 import { defineComponent } from "vue";
 import { useUserStore } from "@/stores/user";
 import axios from "axios";
@@ -30,7 +43,7 @@ import router from "@/router";
 export default defineComponent({
 	name: "TwoFactorLoginView",
 	components: {
-		MainLayout
+		TwoFactorLayout
 	},
 	setup() {
 		const userStore = useUserStore();
@@ -77,7 +90,9 @@ export default defineComponent({
 
 </script>
 
-<style scoped>
+
+<style lang="scss" scoped>
+
 .confirmation-code {
 	display: flex;
 	justify-content: center;
--- project/src/web/app/src/views/2fa/TwoFactorView.vue
+++ project/src/web/app/src/views/2fa/TwoFactorView.vue
@@ -1,7 +1,7 @@
 <template>
   <MainLayout>
-    <div id="two-factor">
-      <div class="content-wrapper flex flex-col items-center">
+    <div id="two-factor" class="main-page-content">
+      <div class="content-wrapper flex flex-col items-center justify-center min-h-full">
         <button v-if="userStore.user.twofaenabled" class="btn text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
           @click="removeTfa">
           Remove 2FA
