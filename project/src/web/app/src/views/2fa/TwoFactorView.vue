8b97fe8d1c675d2b88b029a96089c87938bb6b45
Lucas Dominique <lucasdominique99@gmail.com>
2023-05-02 20:31:24 +0200

    2fa improvements (#35)
    * WIP: tring to use banner when code is invalid on setup
    
    * fix(alert): banner issue on set
    
    * fix(2fa): Fixed redirect when 2fa code invalid, now working with banners
    
    * fix(2fa): removed console logs
    
    ---------
    
    Co-authored-by: MarJC5 <admin@marjc.ch>

--- project/src/web/app/src/views/2fa/TwoFactorView.vue
+++ project/src/web/app/src/views/2fa/TwoFactorView.vue
@@ -1,18 +1,20 @@
 <template>
-  <MainLayout>
-    <div id="two-factor" class="main-page-content">
-      <div class="content-wrapper flex flex-col items-center justify-center min-h-full">
-        <button v-if="userStore.user.twofaenabled" class="btn text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-          @click="removeTfa">
-          Remove 2FA
-        </button>
-        <button v-else-if="!userStore.user.twofaenabled && qrCode == null" class="btn text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2"
-          @click="generateQr">
-          Activate 2FA
-        </button>
-        <div v-if="qrCode" class="flex flex-col items-center mt-4">
-          <img class="h-48 w-48" :src="qrCode" alt="qrCode"/>
-          <div class="flex justify-center mt-4">
+	<MainLayout>
+		<div id="two-factor" class="main-page-content">
+			<div class="content-wrapper flex flex-col items-center justify-center min-h-full">
+				<button v-if="userStore.user.twofaenabled"
+					class="btn text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+					@click="removeTfa">
+					Remove 2FA
+				</button>
+				<button v-else-if="!userStore.user.twofaenabled && qrCode == null"
+					class="btn text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2"
+					@click="generateQr">
+					Activate 2FA
+				</button>
+				<div v-if="qrCode" class="flex flex-col items-center mt-4">
+					<img class="h-48 w-48" :src="qrCode" alt="qrCode" />
+					<div class="flex justify-center mt-4">
             <!-- Use computed property to generate input fields -->
             <input
               v-for="(code, index) in confirmationCode"
@@ -25,90 +27,90 @@
               required
             />
           </div>
-          <button
+					<button
             @click.prevent="confirm"
             class="btn text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-4"
           >
-            Confirm
-          </button>
-        </div>
-      </div>
-    </div>
-  </MainLayout>
+						Confirm
+					</button>
+				</div>
+			</div>
+		</div>
+	</MainLayout>
 </template>
 
 <script lang="ts">
 import MainLayout from "@/components/layout/layout/MainLayout.vue";
-import { defineComponent  } from "vue";
+import { defineComponent } from "vue";
 import { useUserStore } from "@/stores/user";
 import axios from "axios";
 import type { UserInterface } from "@/interfaces/user.interface";
+import { useAlertStore } from "@/stores/alert";
+import type { AlertInterface } from "@/interfaces/alert.interface";
 
 export default defineComponent({
-  name: "TwoFactorView",
-  components: {
-    MainLayout
-  },
-  setup() {
-    const userStore = useUserStore();
+	name: "TwoFactorView",
+	components: {
+		MainLayout
+	},
+	setup() {
+		const userStore = useUserStore();
+		const alertStore = useAlertStore();
 
-    return {
-      userStore
-    };
-  },
-  data() {
-    return {
-      qrCode: null,
-			confirmationCode: Array(6).fill(""),
-    };
-  },
-  methods: {
-    async generateQr() {
-      console.log("2fa");
-
-      axios.post(`${import.meta.env.VITE_APP_API_URL}/2fa/generate`,
-        {
-          withCredentials: true,
-        }
-      ).then((response) => {
-        this.qrCode = response.data.ret;
-        console.log(this.qrCode);
-      }).catch((error) => {
-        console.log(error);
-      });
-    },
+		return {
+			userStore,
+			alertStore
+		};
+	},
+	data() {
+		return {
+			qrCode: null,
+			confirmationCode: Array(6).fill("")
+		};
+	},
+	methods: {
+		async generateQr() {
+			axios.post(`${import.meta.env.VITE_APP_API_URL}/2fa/generate`,
+				{
+					withCredentials: true,
+				}
+			).then((response) => {
+				this.qrCode = response.data.ret;
+			});
+		},
 		async confirm() {
 			const input = this.confirmationCode.join("")
-			console.log("Input Content:", input);
 			axios.post(`${import.meta.env.VITE_APP_API_URL}/2fa/turn-on`,
 				{ tfa_code: input },
-        {
-          withCredentials: true,
-        }
-      ).then((response) => {
-        console.log(response);
-        this.userStore.setUser(response.data.user as UserInterface);
-        this.qrCode = null;
-      }).catch((error) => {
-        console.log(error);
-      });
+				{
+					withCredentials: true,
+				}).then((response) => {
+					if (response.status === 206) {
+						const alert = {
+							status: 401,
+							message: 'Code 2FA incorrect.',
+						} as AlertInterface;
+
+						this.alertStore.setAlert(alert);
+					} else {
+						this.userStore.setUser(response.data.user as UserInterface);
+						this.qrCode = null;
+					}
+				});
 
 		},
 		codeRef(index) {
-      return `code${index + 1}`;
-    },
-    async removeTfa() {
-      axios.post(`${import.meta.env.VITE_APP_API_URL}/2fa/turn-off`,
-        {
-          withCredentials: true,
-        }
-      ).then((response) => {
-        console.log(response);
-        this.userStore.setUser(response.data.user as UserInterface);
-      }).catch((error) => {
-        console.log(error);
-      });
-    }
+			return `code${index + 1}`;
+		},
+		async removeTfa() {
+			axios.post(`${import.meta.env.VITE_APP_API_URL}/2fa/turn-off`,
+				{
+					withCredentials: true,
+				}
+			).then((response) => {
+				this.userStore.setUser(response.data.user as UserInterface);
+			});
+		}
 	}
 
 });
@@ -117,19 +119,19 @@ export default defineComponent({
 
 <style scoped>
 .confirmation-code {
-  display: flex;
-  justify-content: center;
+	display: flex;
+	justify-content: center;
 }
 
 .confirmation-code-box {
-  width: 36px;
-  height: 36px;
-  background-color: #36373a;
-  margin-right: 4px;
-  border-radius: 4px;
+	width: 36px;
+	height: 36px;
+	background-color: #36373a;
+	margin-right: 4px;
+	border-radius: 4px;
 }
 
 .filled {
-  background-color: #3d3f42;
+	background-color: #3d3f42;
 }
 </style>
