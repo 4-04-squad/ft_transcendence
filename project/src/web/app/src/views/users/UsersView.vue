db3f2ee8f32486471d28e36eb50855fedb59fc16
MarJC5 <admin@marjc.ch>
2023-05-21 18:02:06 +0200

    feat(friends): Add the friends interface

--- project/src/web/app/src/views/users/UsersView.vue
+++ project/src/web/app/src/views/users/UsersView.vue
@@ -7,6 +7,7 @@
           <SearchIcon />
           <input type="text" v-model="searchValue" placeholder="Rechercher" />
         </div>
+        <UsersFilters />
       </h1>
     </div>
     <EasyDataTable :headers="headers" :items="items" :theme-color="'var(--primary-color)'" :search-value="searchValue"
@@ -55,6 +56,7 @@ import EasyDataTable from "vue3-easy-data-table";
 import type { UserInterface } from "@/interfaces/user.interface";
 import { SearchIcon, ExternalLinkIcon } from "@/components/icons";
 import FriendRequestButton from "@/components/ui/button/FriendRequestButton.vue";
+import UsersFilters from "@/components/user/UsersFilters.vue";
 
 export default defineComponent({
   name: "UsersView",
@@ -63,6 +65,7 @@ export default defineComponent({
     SearchIcon,
     ExternalLinkIcon,
     FriendRequestButton,
+    UsersFilters,
   },
   setup() {
     const searchValue = ref("");
@@ -76,6 +79,7 @@ export default defineComponent({
       { text: "", value: "profile" },
     ] as Header[];
     const items = ref([] as Item[]);
+
 
     axios
       .get(`${import.meta.env.VITE_APP_API_URL}/users`, {
@@ -109,3 +113,31 @@ export default defineComponent({
   },
 });
 </script>
+
+
+<style lang="scss">
+
+.users-filters {
+    display: flex;
+    align-items: center;
+    margin-left: auto;
+    margin-right: 0;
+    list-style: none;
+    padding: 0;
+    margin: 1.5rem auto 0;
+    font-size: 0.5rem;
+
+    li {
+        margin-left: 1rem;
+        font-weight: bold;
+
+        .btn {
+            &.active {
+                background-color: var(--primary-color);
+                color: #ffffff;
+            }
+        }
+    }
+}
+
+</style>
