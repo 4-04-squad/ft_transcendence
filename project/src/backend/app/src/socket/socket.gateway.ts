ed247aac6e09e02596448235c5704841dc6a4862
Lucas Dominique <lucasdominique99@gmail.com>
2023-05-27 18:53:07 +0200

    #103 - Matchmaking (#102)
    * feat(game): WIP - implementing matchmaking
    
    * WIP
    
    * feat(game): WIP - implementing matchmaking
    
    * feat(game): WIP - added modal for matchmaking

--- project/src/backend/app/src/socket/socket.gateway.ts
+++ project/src/backend/app/src/socket/socket.gateway.ts
@@ -82,25 +82,40 @@ export class SocketsGateway implements OnGatewayInit, OnGatewayConnection, OnGat
     this.logger.log(`Client ${data.user.id} left room ${roomName}`);
   }
 
-
+  /*
+  * Emit action : Matchmaking
+  */
   @SubscribeMessage('waiting')
-  onWaitingGame(@Body() data: { userId: string, status: string }) {
+  onWaitingRoom(@Body() data: { userId: string, gameId?: string }) {
     const roomName = `waiting`;
     // emit in room
-    this.server.to(roomName).emit('waiting', { userId: data.userId, status: data.status });
+    this.server.to(roomName).emit('waiting', { userId: data.userId, gameId: data?.gameId });
+    this.logger.log(`Client ${data.userId} waiting`);
   }
 
   @SubscribeMessage('joinWaitingGame')
-  onJoinWaitingGame(client: Socket, data: { userId: string }) {
+  onJoinWaitingRoom(client: Socket, data: { userId: string }) {
     const roomName = `waiting`;
-    const userRooms = this.connectedRooms.get(data.userId) || new Set<string>();
-
-    if (userRooms.has(roomName)) {
-      return;
-    }
 
     client.join(roomName);
+
     this.logger.log(`Client ${data.userId} joined room ${roomName}`);
+  }
+
+  leaveSocketWaiting(userId: string) {
+    const roomName = `waiting`;
+    this.server.to(roomName).emit('leaveWaiting', { userId });
+  }
+
+  @SubscribeMessage('leaveWaiting')
+  onLeaveWaiting(client: Socket, data: { userId: string }) {
+    const roomName = `waiting`;
+
+    client.leave(roomName);
+	
+    this.logger.log(`Client ${data.userId} left waiting room`);
+
+    this.leaveSocketWaiting(data.userId);
   }
 
 
---
+++ project/src/web/app/src/components/games/MatchmakingModal.vue
@@ -0,0 +1,100 @@
+<template>
+	<div class="modal-container">
+	  <div class="modal-backdrop" @click="closeModal"></div>
+	  <form class="modal">
+			<h1 class="modal-title">Looking for an opponent</h1>
+            <div class="btns modal-action">
+                <button class="btn btn--delete" @click="closeModal">Cancel</button>
+            </div>
+        </form>
+	</div>
+</template>
+  
+<script lang="ts">
+import { useUserStore } from "@/stores/user";
+import { defineComponent, inject } from "vue";
+import type { Socket } from "socket.io-client";
+
+  export default defineComponent({
+	name: "MatchmakingModal",
+	setup(props, { emit }) {
+        const userStore = useUserStore();
+        const socket = inject('socket') as Socket;
+
+		const closeModal = () => {
+            socket.emit("leaveWaiting", {
+                        userId: userStore.user.id,
+                    })
+			emit("onClose", false);
+		};
+
+		return {
+			closeModal,
+            userStore
+    };
+
+	},
+  });
+</script>
+  
+  <style lang="scss">
+  .modal-container {
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	z-index: 1000;
+  }
+  
+  .modal-backdrop {
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.5);
+	z-index: 1001;
+  }
+  
+  .modal {
+	width: 30rem;
+	background-color: var(--secondary-color);
+	border-radius: 0.6rem;
+	padding: 1rem;
+	z-index: 1002;
+	label{
+		font-weight: bold;
+	}
+
+	.modal-action{
+		display: flex;
+		gap: 1rem;
+		justify-content: flex-end;
+	}
+  }
+  
+  .modal-title {
+	margin-bottom: 1rem;
+	text-align: center;
+  }
+  
+  .btn {
+	&--cancel {
+	  background-color: var(--danger-color);
+	  color: #fff;
+	  border: none;
+	  padding: 10px 20px;
+	  cursor: pointer;
+	  transition: background-color 0.3s ease;
+  
+	  &:hover {
+		background-color: var(--danger-color);
+	  }
+	}
+  }
+
+</style>
