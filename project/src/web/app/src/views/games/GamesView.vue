9828e762db4389005367315f1f5f7ec22da4cc16
yace1 <yacinbent@gmail.com>
2023-05-29 17:56:24 +0200

    fix(games): remove watch mode

--- project/src/web/app/src/views/games/GamesView.vue
+++ project/src/web/app/src/views/games/GamesView.vue
@@ -45,6 +45,7 @@ export default defineComponent({
       updatedAt.value = Date.now();
     });
 
+
     const fetchChatDataAndJoinGame = async (gameId: string) => {
       try {
         const response = await axios.get(
@@ -66,6 +67,10 @@ export default defineComponent({
             name: "games",
           });
         }
+        if (!users.value.some((u) => u.id === userStore.user.id))
+          router.push({
+            name: "games",
+        });
         if (gameData.value?.status === "FINISHED") {
           const alert = {
             status: 401,
